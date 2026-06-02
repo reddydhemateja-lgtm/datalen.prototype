254bca627d19d1a6a35372125635afeb70ffbb65
reddydhemateja-lgtm <reddydhemateja@gmail.com>
2026-06-02 12:16:42 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.static(path.join(__dirname, '../../frontend')));
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../frontend/index.html'));
+  //res.sendFile(path.join(__dirname, '../../frontend/index.html'));
 });
 
 const storage = multer.memoryStorage();
